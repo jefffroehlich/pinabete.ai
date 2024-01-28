3bf8ede84980019db8585bb3dbc55745a9542391
Jeff Froehlich <froehlich.jeff@gmail.com>
2024-01-28 12:03:05 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const inputEl = document.getElementById('input-el');
+const btnEl = document.getElementById('btn-el');
+
+btnEl.addEventListener('click', () => {
+  sendData(inputEl.value);
+  inputEl.value = '';
+});
+
+inputEl.addEventListener('keydown', function (event) {
+  // Check if the pressed key is Enter
+  if (event.key === 'Enter') {
+    sendData(inputEl.value);
+    inputEl.value = '';
+  }
+});
+
+let clientSideMessage = [];
+
+async function sendData(userInput) {
+  clientSideMessage.push({ role: 'user', content: userInput });
+
+  const response = await fetch('http://localhost:3000/sendUserInput', {
+    method: 'POST',
+    body: JSON.stringify({
+      id: 1,
+      message: clientSideMessage,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  if (response.ok) {
+    const data = await response.json();
+    clientSideMessage.push({ role: 'assistant', content: data });
+    console.log(data);
+    const promptDiv = document.createElement('div');
+    promptDiv.innerText = userInput;
+    promptDiv.classList.add('prompt');
+
+    const responseDiv = document.createElement('div');
+    responseDiv.innerText = data;
+    responseDiv.classList.add('response');
+
+    const responseContainer = document.getElementById('response-container');
+    responseContainer.prepend(responseDiv);
+    responseContainer.prepend(promptDiv);
+  }
+}
---
+++ server.js
@@ -0,0 +1,46 @@
+import { config } from 'dotenv';
+config();
+
+import { Configuration, OpenAIApi } from 'openai';
+import readline from 'readline';
+
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = 3000;
+app.use(cors({ origin: 'http://127.0.0.1:5500' }));
+app.use(express.json());
+app.listen(port, () => console.log('Example app listening on port 3000!'));
+
+app.post('/sendUserInput', async (req, res) => {
+  const dataFromClientSide = req.body.message;
+
+  let promptObj = {
+    model: 'gpt-3.5-turbo',
+    messages: [
+      {
+        role: 'system',
+        content:
+          'You are a very rigid assistant that always uses 5 word responses.',
+      },
+    ],
+  };
+  //loop pushes in prompt and response into the object being passed to openai
+  for (let i = 0; i < dataFromClientSide.length; i++) {
+    promptObj.messages.push(dataFromClientSide[i]);
+  }
+
+  const gptResponse = await talkToChatGPT(promptObj);
+  res.status(200).send(JSON.stringify(gptResponse));
+});
+
+const openAi = new OpenAIApi(
+  new Configuration({
+    apiKey: process.env.OPEN_AI_API_KEY,
+  })
+);
+async function talkToChatGPT(fullPrompt) {
+  const response = await openAi.createChatCompletion(fullPrompt);
+  return response.data.choices[0].message.content;
+}
