70213b42a7064a94ba71707a75b52e285804687b
Jeff Froehlich <froehlich.jeff@gmail.com>
2024-01-28 13:59:13 -0800

    env port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 app.use(cors({ origin: 'https://jefffroehlich.github.io' }));
 app.use(express.json());
 app.listen(port, () => console.log('Example app listening on port 3000!'));
